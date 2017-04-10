85317f06edfed312c979b497484f9e0981bb383b
cuiqiusheng <363092524@qq.com>
2017-04-10 17:41:05 +0800

    ejs

--- index.js
+++ index.js
@@ -7,16 +7,13 @@ var connection = mysql.createConnection({
     password: '',
     database:'jacob'
 });
-var ejs = require('ejs');
 var app = express();
 
 connection.connect();
 // 查询
 connection.query('SELECT * from `main`', function(err, rows, fields) {
   if (err) throw err;
-  console.log('**************************************');
   console.log(rows);
-  console.log('**************************************');
 });
 //关闭连接
 connection.end();
@@ -26,23 +23,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎
-app.engine('html', ejs.__express);
-app.set('view engine', 'html');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
   res.redirect('login');
 });
 // 登录页
 app.get('/login', function(req, res) {
-  res.render('login');
+  res.render('login', {title: '登录'});
 });
 // 注册页
 app.get('/register', function(req, res) {
-  res.render('register');
+  res.render('register', {title: '注册'});
 });
 // 登陆成功
 app.get('/content', function(req, res) {
-  res.render('content');
+  res.render('content', {title: '小明书屋'});
 });
 // 监听8000端口
 app.listen(8000);
