9908b0d4d72055c3414700ad1030dd94f3405ab9
cuiqiusheng <363092524@qq.com>
2017-04-11 17:18:57 +0800

    sessionID

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var path = require('path');
 var express = require('express');
-var app = express();
+var router = express.Router();
 var mysql = require('mysql');
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
 var flash = require('connect-flash');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+
 var checkLogin = require('./middlewares/check').checkLogin;
 var checkNotLogin = require('./middlewares/check').checkNotLogin;
+
+var app = express();
+
+// 设置模板目录
+app.set('views', path.join(__dirname, 'views'));
+// 设置模板引擎
+app.set('view engine', 'ejs');
+
+// 设置静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 配置数据库
 var options = {
     host: 'localhost',
     port: 3306,
@@ -24,9 +37,7 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(multer()); // for parsing multipart/form-data
 
-// 设置静态文件目录
-app.use(express.static(path.join(__dirname, 'public')));
-
+// session中间件
 app.use(session({
   name: 'demo',
   secret: 'guess',
@@ -40,47 +51,54 @@ app.use(session({
 
 app.use(flash());
 
-// 设置模板目录
-app.set('views', path.join(__dirname, 'views'));
-// 设置模板引擎
-app.set('view engine', 'ejs');
+app.use(router);
 
 app.get('/', function(req, res) {
   res.redirect('login');
 });
 // 登录页
-app.get('/login', function(req, res) {
+router.get('/login', function(req, res) {
   res.render('login', {title: '登录'});
 });
 // 注册页
-app.get('/register', function(req, res) {
+router.get('/register', function(req, res) {
   res.render('register', {title: '注册'});
+  req.flash('info', '请输入用户名');
 });
-app.post('/register', function(req, res, next) {
+
+router.post('/register', function(req, res, next) {
   var username = req.body.username;
   var password = req.body.password;
   var repassword = req.body.repassword;
-  if(!username) {
-    req.flash('error', '请输入用户名');
-    return;
+  try {
+    if(!username) {
+      throw new Error('请输入用户名');
+    }
+    if(!password) {
+      throw new Error('请输入密码');
+    }
+    if(password !== repassword) {
+      throw new Error('两次输入密码不一致');
+    }
+  } catch(e) {
+    console.log('验证不通过：' + e);
   }
-  if(!password) {
-    req.flash('error', '请输入密码');
-    return;
-  }
-  if(password !== repassword) {
-    req.flash('error', '两次输入密码不一致');
-    return;
-  }
+  console.log('req.session：' + req.session);
   connection.query({
-    sql: `INSERT INTO user (username, password) VALUES (${username}, ${password})`},
+    sql: `INSERT INTO user (id, username, password) VALUES ('${req.sessionID}', '${username}', '${password}');`},
+    console.log('sql语句是：' + connection.query.sql),
     function(error, results, fields) {
-      console.log(results);
+      if(error) throw error;
+      console.log('results：' + results);
+      req.session.id = req.sessionID;
+      req.session.user = username;
+      res.send('注册成功');
     }
   );
+  // connection.end();
 });
 // 登陆成功
-app.get('/content', function(req, res) {
+router.get('/content', function(req, res) {
   res.render('content', {title: '小明书屋'});
 });
 
